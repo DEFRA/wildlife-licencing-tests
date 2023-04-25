07323e832bf5ed08473610675e577a89a1c7841c
Dele Lookman <Dele.Lookman@defra.gov.uk>
2023-04-25 08:06:15 +0100

    test in progress

--- cypress/e2e/firstApplicationwithnodata.js
+++ cypress/e2e/firstApplicationwithnodata.js
@@ -13,8 +13,8 @@ describe("Page title verification", () => {
     );
     cy.visit(Cypress.env("baseUrl") + Cypress.env("login"));
     cy.fixture("users.json").then((users) => {
-      cy.get("#username").type(users.email3);
-      cy.get("#password").type(users.password2);
+      cy.get("#username").fill(users.email3);
+      cy.get("#password").fill(users.password2);
       cy.get("#continue").click();
       cy.get("#species").click();
       cy.get("#continue").click();
@@ -31,15 +31,15 @@ describe("Page title verification", () => {
       cy.get("a").contains("Give licence holder details").click();
       cy.get("#yes-no").click();
       cy.get("#continue").click();
-      cy.get("#name").type(
+      cy.get("#name").fill(
         faker.name.firstName() + " " + faker.name.lastName()
       );
       cy.get("#continue").click();
       cy.get("#is-organisation").click();
-      cy.get("#organisation-name").type(faker.company.bs());
+      cy.get("#organisation-name").fill(faker.company.bs());
       cy.get("#continue").click();
       cy.get("#continue").click();
-      cy.get("#postcode").type("M24 6DH");
+      cy.get("#postcode").fill("M24 6DH");
       cy.get("#continue").click();
       cy.get("#address").select(
         "1, SATIN DRIVE, MIDDLETON, MANCHESTER, M24 6DH"
@@ -51,11 +51,11 @@ describe("Page title verification", () => {
       cy.get("#yes-no").click();
       cy.get("#continue").click();
       cy.get("#is-organisation").click();
-      cy.get("#organisation-name").type(faker.company.bs());
+      cy.get("#organisation-name").fill(faker.company.bs());
       cy.get("#continue").click();
       cy.get("#change-email").click();
       cy.get("#continue").click();
-      cy.get("#postcode").type("M24 6DH");
+      cy.get("#postcode").fill("M24 6DH");
       cy.get("#continue").click();
       cy.get("#address").select(
         "1, SATIN DRIVE, MIDDLETON, MANCHESTER, M24 6DH"
@@ -67,13 +67,13 @@ describe("Page title verification", () => {
       cy.get("a").contains("Add authorised people").click();
       cy.get("#yes-no").click();
       cy.get("#continue").click();
-      cy.get("#name").type(
+      cy.get("#name").fill(
         faker.name.firstName() + " " + faker.name.lastName()
       );
       cy.get("#continue").click();
-      cy.get("#email-address").type(faker.internet.email());
+      cy.get("#email-address").fill(faker.internet.email());
       cy.get("#continue").click();
-      cy.get("#postcode").type("M24 6DH");
+      cy.get("#postcode").fill("M24 6DH");
       cy.get("#continue").click();
       cy.get("#address").select(
         "1, SATIN DRIVE, MIDDLETON, MANCHESTER, M24 6DH"
@@ -86,35 +86,35 @@ describe("Page title verification", () => {
       cy.get("a").contains("Add additional contacts").click();
       cy.get("#yes-no").click();
       cy.get("#continue").click();
-      cy.get("#name").type(
+      cy.get("#name").fill(
         faker.name.firstName() + " " + faker.name.lastName()
       );
       cy.get("#continue").click();
-      cy.get("#email-address").type(faker.internet.email());
+      cy.get("#email-address").fill(faker.internet.email());
       cy.get("#continue").click();
       cy.get("#yes-no").click();
       cy.get("#continue").click();
-      cy.get("#name").type(faker.name.firstName() + faker.name.lastName());
+      cy.get("#name").fill(faker.name.firstName() + faker.name.lastName());
       cy.get("#continue").click();
       cy.get("#email-address").clear();
-      cy.get("#email-address").type(faker.internet.email());
+      cy.get("#email-address").fill(faker.internet.email());
       cy.get("#continue").click();
       cy.get("#continue").click();
 
       cy.get("a").contains("Add invoice details").click();
       cy.get("input[value='other']").click();
       cy.get("#continue").click();
-      cy.get("#name").type(faker.name.firstName() + faker.name.lastName());
+      cy.get("#name").fill(faker.name.firstName() + faker.name.lastName());
       cy.get("#continue").click();
       cy.get("#new").click();
       cy.get("#continue").click();
       cy.get("#is-organisation").click();
-      cy.get("#organisation-name").type(faker.company.bs());
+      cy.get("#organisation-name").fill(faker.company.bs());
       cy.get("#continue").click();
       cy.get("#email-address").clear();
-      cy.get("#email-address").type(faker.internet.email());
+      cy.get("#email-address").fill(faker.internet.email());
       cy.get("#continue").click();
-      cy.get("#postcode").type("M24 6DH");
+      cy.get("#postcode").fill("M24 6DH");
       cy.get("#continue").click();
       cy.get("#address").select(
         "12, SATIN DRIVE, MIDDLETON, MANCHESTER, M24 6DH"
@@ -123,12 +123,12 @@ describe("Page title verification", () => {
       cy.get("#continue").click();
       // work activity
       cy.get("a").contains("Give work activity details").click();
-      cy.get("#work-proposal").type(faker.random.alphaNumeric(40));
+      cy.get("#work-proposal").fill(faker.random.alphaNumeric(40));
       cy.get("#continue").click();
       cy.get("#yes-no").click();
       cy.get("#continue").click();
       cy.get("#work-payment-exempt-reason-9").click();
-      cy.get("#exempt-details").type(faker.random.alphaNumeric(400));
+      cy.get("#exempt-details").fill(faker.random.alphaNumeric(400));
       cy.get("#continue").click();
       cy.get("#work-category-13").click();
       cy.get("#continue").click();
@@ -144,20 +144,20 @@ describe("Page title verification", () => {
       cy.get("#yes-no").click();
       cy.get("#continue").click();
       cy.get("#continue").click();
-      cy.get("#site-name").type("a");
+      cy.get("#site-name").fill("a");
       cy.get("#site-name__option--0").click();
       cy.get("#continue").click();
       cy.get("#yes-no").click();
       cy.get("#continue").click();
-      cy.get("#permission-details").type(faker.random.alphaNumeric(40));
+      cy.get("#permission-details").fill(faker.random.alphaNumeric(40));
       cy.get("#continue").click();
       cy.get("#yes-no").click();
       cy.get("#continue").click();
-      //   cy.get("#advice-from-who").type(
+      //   cy.get("#advice-from-who").fill(
       //     faker.name.firstName() + faker.name.lastName()
       //   );
-      cy.get("#advice-from-who").type(faker.random.alphaNumeric(40));
-      cy.get("#advice-description").type(faker.random.alphaNumeric(40));
+      cy.get("#advice-from-who").fill(faker.random.alphaNumeric(40));
+      cy.get("#advice-description").fill(faker.random.alphaNumeric(40));
       cy.get("#continue").click();
       cy.get("#proximity").click();
       cy.get("#continue").click();
@@ -168,34 +168,34 @@ describe("Page title verification", () => {
 cy.get("a").contains("Give details of permissions to start work").click();
 cy.get("#continue").click();
 cy.get("#continue").click();
-cy.get("#consent-type-check").click(); 
+cy.get("#consent-fill-check").click(); 
 cy.get("#continue").click();
-cy.get("#authority-name").type("a");
+cy.get("#authority-name").fill("a");
 cy.get("#authority-name__option--0").click(); 
 cy.get("#continue").click();
-cy.get("#planning-type").click();
+cy.get("#planning-fill").click();
 cy.get("#continue").click();
-cy.get("#reference").type(faker.name.firstName());
+cy.get("#reference").fill(faker.name.firstName());
 cy.get("#continue").click();
 cy.get("#add-another-permission-2").click();
 cy.get("#continue").click();
 cy.get("#conditions-met-2").click();
 cy.get("#continue").click();
-cy.get("#conditions-not-met-reason").type(faker.lorem.words(5));
+cy.get("#conditions-not-met-reason").fill(faker.lorem.words(5));
 cy.get("#continue").click();
 cy.get("#potential-conflicts").click();
 cy.get("#continue").click();
-cy.get("#describe-potential-conflicts").type(faker.lorem.words(4));
+cy.get("#describe-potential-conflicts").fill(faker.lorem.words(4));
 cy.get("#continue").click();
 cy.get("#continue").click();
 cy.get("#main-content > fieldset > span > ol > li:nth-child(3) > ul > li:nth-child(3)").contains("COMPLETED");
 cy.get("#main-content > fieldset > span > ol > li:nth-child(3) > ul > li:nth-child(3)").contains("Give details of permissions to start work");
   // site info
       cy.get("a").contains("Give site information").click();
-      cy.get("#site-name").type("test");
+      cy.get("#site-name").fill("test");
       cy.get("#continue").click();
       cy.get("#site-postcode-check").click();
-      cy.get("#site-postcode").type("B38 9SH");
+      cy.get("#site-postcode").fill("B38 9SH");
       cy.get("#continue").click();
       cy.get("#siteAddress").select("2, BAY TREE CLOSE, BIRMINGHAM, B38 9SH");
       cy.get("#continue").click();
@@ -209,20 +209,20 @@ cy.get("#main-content > fieldset > span > ol > li:nth-child(3) > ul > li:nth-chi
       cy.get("#scan-file").click();
       cy.get("#scan-file").attachFile(yourFixturePath);
       cy.get("#continue").click();
-      cy.get("#site-grid-ref").type("SP039775");
+      cy.get("#site-grid-ref").fill("SP039775");
       cy.get("#continue").click();
       cy.get("#continue").click();
 
       cy.get("a").contains("Give ecologist’s experience").click();
       cy.get("input[value='no']").click();
       cy.get("#continue").click();
-      cy.get("#enter-experience").type(faker.random.alphaNumeric(20));
+      cy.get("#enter-experience").fill(faker.random.alphaNumeric(20));
       cy.get("#continue").click();
-      cy.get("#enter-methods").type(faker.random.alphaNumeric(21));
+      cy.get("#enter-methods").fill(faker.random.alphaNumeric(21));
       cy.get("#continue").click();
       cy.get("input[value='yes']").click();
       cy.get("#continue").click();
-      cy.get("#enter-class-mitigation-details").type("BAD-2022-agsss1");
+      cy.get("#enter-class-mitigation-details").fill("BAD-2022-agsss1");
       cy.get("#continue").click();
       cy.get("#continue").click();
 
@@ -239,26 +239,26 @@ cy.get("#main-content > fieldset > span > ol > li:nth-child(3) > ul > li:nth-chi
           cy.get("#continue").click();
           cy.get("a").contains("Add setts the work will affect").click();
           cy.get("#continue").click();
-          cy.get("input[id='habitat-name']").type(faker.name.firstName());
+          cy.get("input[id='habitat-name']").fill(faker.name.firstName());
           cy.get("#continue").click();
           cy.get("#habitat-types-3").click();
           cy.get("#continue").click();
           cy.get("input[value='true']").click();
           cy.get("#continue").click();
-          cy.get("#habitat-entrances").type("7");
+          cy.get("#habitat-entrances").fill("7");
           cy.get("#continue").click();
-          cy.get("#habitat-active-entrances").type("5");
+          cy.get("#habitat-active-entrances").fill("5");
           cy.get("#continue").click();
           cy.get("#habitat-grid-ref").clear();
-          cy.get("#habitat-grid-ref").type("NY123456");
+          cy.get("#habitat-grid-ref").fill("NY123456");
           cy.get("#continue").click();
-          cy.get("#habitat-work-start-day").type("1");
-          cy.get("#habitat-work-start-month").type("11");
-          cy.get("#habitat-work-start-year").type("2024");
+          cy.get("#habitat-work-start-day").fill("1");
+          cy.get("#habitat-work-start-month").fill("11");
+          cy.get("#habitat-work-start-year").fill("2024");
           cy.get("#continue").click();
-          cy.get("#habitat-work-end-day").type("30");
-          cy.get("#habitat-work-end-month").type("11");
-          cy.get("#habitat-work-end-year").type("2024");
+          cy.get("#habitat-work-end-day").fill("30");
+          cy.get("#habitat-work-end-month").fill("11");
+          cy.get("#habitat-work-end-year").fill("2024");
           cy.get("#continue").click();
           cy.get("#habitat-activities").click();
           cy.get("#continue").click();
